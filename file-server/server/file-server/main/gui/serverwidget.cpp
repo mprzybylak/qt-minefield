e78a32211055968b80c3b6a9a9eeb220c18ff19c
Michal Przybylak <michal.przybylak@gmail.com>
2015-01-14 22:04:50 +0100

    files list in directory

--- file-server/server/file-server/main/gui/serverwidget.cpp
+++ file-server/server/file-server/main/gui/serverwidget.cpp
@@ -1,6 +1,8 @@
 #include "serverwidget.h"
 #include "ui_serverwidget.h"
 #include <QFileDialog>
+#include <QDirIterator>
+#include <QDebug>
 
 ServerWidget::ServerWidget(SetupService* setupService, QWidget *parent) :
     QWidget(parent),
@@ -32,6 +34,19 @@ void ServerWidget::selectDirectory()
     QString dir = QFileDialog::getExistingDirectory(this, tr("Open Directory"), "/home", QFileDialog::ShowDirsOnly | QFileDialog::DontResolveSymlinks);
     setupService->selectDirectoryToServe(dir);
     ui->selectedDirectoryPath->setText(dir);
+
+    ui->fileTable->clear();
+    ui->fileTable->setRowCount(0);
+
+    QDirIterator it(dir);
+
+    while(it.hasNext()) {
+        it.next();
+        qDebug() << it.fileInfo().fileName();
+        QTableWidgetItem* item = new QTableWidgetItem(it.fileInfo().fileName());
+        ui->fileTable->insertRow(ui->fileTable->rowCount());
+        ui->fileTable->setItem(ui->fileTable->rowCount()-1, 0, item);
+    }
 }
 
 ServerWidget::~ServerWidget()
